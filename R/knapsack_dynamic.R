85ea798f1640edb5ab667d72be0d1bc9a0e72fe6
Kristina Levina <existentialantiproton@protonmail.com>
2021-10-06 12:24:45 +0200

    solution found

--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -4,7 +4,7 @@ set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
 n <- 16
 knapsack_objects <- 
   data.frame(
-    w = sample(1:10, size = n, replace = TRUE), v = runif(n = n, 0, 10000))
+    w = sample(1:4000, size = n, replace = TRUE), v = runif(n = n, 0, 10000))
 
 
 knapsack_dynamic <- function(cx, W){
@@ -17,7 +17,7 @@ knapsack_dynamic <- function(cx, W){
   if(min(cx$v) < 0) {stop("Vector 'v' is not all positive!")}
   if(min(cx$w) < 0) {stop("Vector 'w' is not all positive!")}
   
-  print(cx)
+  #print(cx)
   
   #create the tabulation matrix:
   m_tabulation <- matrix(data = 0, nrow = n_items + 1, ncol = W + 1, byrow = FALSE, dimnames = NULL)
@@ -42,14 +42,32 @@ knapsack_dynamic <- function(cx, W){
   for (items in 2:(n_items + 1)) {
     for (tabulated_weight in 2:(W + 1)) {
       tabulation_formula(items, tabulated_weight)
-      print(m_tabulation)
+      #print(m_tabulation)
     }
   }
 
   #find the best solution from the table
+  n_max_value <- max(m_tabulation)
+  #print(n_max_value)
+  n_value_subtracted <- n_max_value
   
+  #Find which objects are included
+  v_included_objects <- c()
+  for (item in 1:n_items) {
+    if(length(which(m_tabulation[n_items + 1 - item,] == n_value_subtracted)) > 0){
+      next
+    }
+    else{
+      n_value_subtracted <- n_value_subtracted - cx$v[n_items + 1 - item]
+      v_included_objects <- c(v_included_objects, n_items + 1 - item)
+    }
+  }
+  
+  #print(v_included_objects)
   #calculate the running time
+  
+  return(list("value" = n_max_value, "elements" = sort.int(v_included_objects)))
 }
 
 
-knapsack_dynamic(cx = knapsack_objects[1:4, ], W = 10)
+knapsack_dynamic(cx = knapsack_objects[1:8, ], W = 3500)
