bc5d1a7c13c270a59b590434472789b7d830b582
Kristina Levina <existentialantiproton@protonmail.com>
2021-10-06 10:31:18 +0200

    dynamic tabulator function pseudo-code is generated

---
+++ R/knapsack_dynamic.R
@@ -0,0 +1,15 @@
+knapsack_dynamic <- function(x, W){
+  
+  #check the input
+  
+  #write the recursive formula into the recursive function:
+  
+  #call the function recursively until the table is filled
+  
+  #find the best solution from the table
+  
+  #calculate the running time
+}
+
+
+knapsack_dynamic(cx = knapsack_objects[1:8, ], W = 3500)
